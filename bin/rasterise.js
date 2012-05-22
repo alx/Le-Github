45af34bf819eccf71ab1a8871e99c0f0cbd844a4
Alexandre Girard <git@alexgirard.com>
2012-05-22 11:46:16 +0200

    prepare script for parameters input

--- bin/rasterise.js
+++ bin/rasterise.js
@@ -1,18 +1,23 @@
 var page = new WebPage(),
     address, output, size;
 
-page.viewportSize = { width: 384, height: 10 };
-page.open("index.html", function (status) {
-  if (status !== 'success') {
-    console.log('Unable to load the address "' + address + '"');
-    phantom.exit(1);
-  } else {
-    page.evaluate(function() {
-      document.getElementsByTagName("body")[0].className = ""
-    });
-    window.setTimeout(function () {
-      page.render("output.jpg");
-      phantom.exit();
-    }, 200);
-  }
-});
+if (phantom.args.length != 1 ) {
+  console.log('Usage: rasterize.js YYYY-MM-DD');
+  phantom.exit();
+} else {
+  page.viewportSize = { width: 384, height: 10 };
+  page.open("index.html?date=" + phantom.args[0], function (status) {
+    if (status !== 'success') {
+      console.log('Unable to load the address "' + address + '"');
+      phantom.exit(1);
+    } else {
+      page.evaluate(function() {
+        document.getElementsByTagName("body")[0].className = ""
+      });
+      window.setTimeout(function () {
+        page.render("output.jpg");
+        phantom.exit();
+      }, 200);
+    }
+  });
+};
